9686b5265203822790b97f3d643c3a75322d1679
abdalla1923 <alesaabdalla@gmail.com>
2023-03-15 20:38:29 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+AOS.init({
+    duration: 2000,
+    once: true,
+  })
+
+  function toggleZoomScreen() {
+    document.body.style.zoom = "80%";
+  } 
+  
+  toggleZoomScreen() 
+
+  /* //////////////////////////////////////////// */
+
